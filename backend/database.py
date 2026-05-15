54ed98abadb720dd3d2b50f041e5c849de18fb29
ABSPL188 <ABSPL188@aurarcm.org>
2026-05-16 03:18:28 +0530

    db in supabase

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:Karthick77@localhost/eshop"
+DATABASE_URL = "postgresql+psycopg2://postgres.dlohtdpjylitmrmeykem:Karthick%407777@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(
     DATABASE_URL,
